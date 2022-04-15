56f1e3abe5dce0cca3b5dc09a38a8c3bfc82b59a
r_samohval <rsamohvwal@gmail.com>
2022-04-15 12:11:06 +0300

    Добавил создание и подключение к локальной базе данных mongoDB

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const Application = require('./framework/Application')
 const userRouter = require('./src/user-router')
 const parseJson = require('./framework/parseJson')
 const parseUrl = require('./framework/parseUrl')
-
+const mongoose = require('mongoose');
 const app = new Application()
-
+const config = require('./config')
 app.addRouter(userRouter)
 
 const PORT = process.env.PORT || 5000
@@ -12,6 +12,23 @@ const PORT = process.env.PORT || 5000
 app.use(parseJson)
 app.use(parseUrl('http://localhost:5000'))
 
-app.listen(PORT, () => {
-    console.log(`Server started on Port ${PORT}`)
-})
+// app.listen(PORT, () => {
+//     console.log(`Server started on Port ${PORT}`)
+// })
+
+const start = async () => {
+    try {
+        await mongoose.connect(config.dbUrl)
+        mongoose.connection.on('connected', () => {
+            console.log('connected to mongo database')
+        })
+        app.listen(PORT, () => console.log(`Server started on Port ${PORT}`))
+    } catch (e) {
+        mongoose.connection.on('error', (err) => {
+            console.log('Error at mongoDB ' + err)
+        })
+        console.log(e)
+    }
+}
+
+start()
